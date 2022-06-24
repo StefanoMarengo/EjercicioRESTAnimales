d3a008b88e5e528e2b84e0fc45efb23e9b8cdb88
DESKTOP-BF35AJB\Usuario <stefmar23ivan@gmail.com>
2022-06-24 19:05:39 -0300

    dfsdf

---
+++ Entidades/Animal.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Animal
+    {
+        // ID; Nombre, Especie, Fecha Creación, Fecha modificación, Eliminado = true / false.
+        public int ID { get; set; }
+        public string Nombre { get; set; }
+        public string Especie { get; set; }
+        public int Edad { get; set; }
+        public DateTime FechaCreacion { get; set; }
+        public DateTime FechaModif { get; set; }
+        public bool Eliminado { get; set; }
+
+    }
+}
---
+++ Entidades/Lista.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    class Lista
+    {
+        private static List<Animal> ListaAnimales = new List<Animal>()
+        {
+            new Animal(){ID=0, Nombre="Morita", Especie="Caniche", FechaCreacion=DateTime.Parse("20/06/2022"), FechaModif=DateTime.Parse("23/06/2022"), Eliminado=false},
+            new Animal(){ID=1, Nombre="Poroto", Especie="Gato", FechaCreacion=DateTime.Parse("20/06/2022"), FechaModif=DateTime.Parse("23/06/2022"), Eliminado=false},
+            new Animal(){ID=2, Nombre="Moro", Especie="Perro", FechaCreacion=DateTime.Parse("20/06/2022"), FechaModif=DateTime.Parse("23/06/2022"), Eliminado=true},
+            new Animal(){ID=3, Nombre="Oli", Especie="Perro", FechaCreacion=DateTime.Parse("20/06/2022"), FechaModif=DateTime.Parse("23/06/2022"), Eliminado=false},
+            new Animal(){ID=4, Nombre="Samantha", Especie="Perro", FechaCreacion=DateTime.Parse("20/06/2022"), FechaModif=DateTime.Parse("23/06/2022"), Eliminado=false}
+        };
+    }
+}
